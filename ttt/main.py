c1e5d44a85b16f749cb6221b178dc0dab6b5018a
Daniel Fackrell <dfackrell@bluehost.com>
2015-02-03 08:14:59 -0700

    Moved ttt to its own directory

--- ttt/main.py
+++ ttt/main.py
@@ -4,11 +4,13 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.clock import Clock
 
-in_game = True
+state = Object()
+state.in_game = True
+state.current_player = False
+state.board = [None] * 9
+state.root = None
+
 players = ['X', 'O']
-current_player = False
-board = [None] * 9
-root = None
 winning_directions = [
         [1, 3, 4],
         [3],
@@ -23,35 +25,31 @@ winning_directions = [
 
 class TicTacToeApp(App):
     def build(self):
-        global root
-        root = Builder.load_file('ttt.kv')
-        return root
+        state.root = Builder.load_file('main.kv')
+        return state.root
 
 def new_game(self, root):
-    global in_game
-    global current_player
     board_widget = root.ids.board
-    for square_num in range(len(board)):
-        board[square_num] = None
+    for square_num in range(len(state.board)):
+        state.board[square_num] = None
         board_widget.children[square_num].text = ''
         board_widget.children[square_num].background_color = [0.25, 0, 0.25, 1]
-    in_game = True
-    current_player = False
-    root.ids.status.text = 'Turn: {}'.format(players[current_player])
+    state.in_game = True
+    state.current_player = False
+    root.ids.status.text = 'Turn: {}'.format(players[state.current_player])
 
 def check_winner(root):
-    global in_game
     open_squares = 0
     board_widget = root.ids.board
-    for square in range(len(board)):
-        player = board[square]
+    for square in range(len(state.board)):
+        player = state.board[square]
         if player is None:
             open_squares += 1
             continue
         for direction in winning_directions[square]:
-            if (player == board[square + direction]
-                    and player == board[square + 2 * direction]):
-                in_game = False
+            if (player == state.board[square + direction]
+                    and player == state.board[square + 2 * direction]):
+                state.in_game = False
                 for button_number in (square,
                         square + direction,
                         square + 2 * direction):
@@ -64,19 +62,18 @@ def check_winner(root):
                 root.ids.status.text = '{} wins!'.format(players[player])
     if open_squares < 1:
         root.ids.status.text = 'Tie!'
-        in_game = False
+        state.in_game = False
 
 def press(button, root):
-    global current_player
-    if not in_game:
+    if not state.in_game:
         return
     square = button.square
-    if board[square] is not None:
+    if state.board[square] is not None:
         return
-    button.text = players[current_player]
-    board[square] = current_player
-    current_player = not current_player
-    root.ids.status.text = 'Turn: {}'.format(players[current_player])
+    button.text = players[state.current_player]
+    state.board[square] = state.current_player
+    state.current_player = not state.current_player
+    root.ids.status.text = 'Turn: {}'.format(players[state.current_player])
     check_winner(root)
 
 if __name__ == '__main__':
